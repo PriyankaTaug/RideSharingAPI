2fd717937e942376310bef579ac664fea0345d61
unknown <aghilafalgunana@gmail.com>
2025-02-10 23:45:01 +0530

    Completed user setup

--- RideSharingapp/serializer.py
+++ RideSharingapp/serializer.py
@@ -1,7 +1,38 @@
 from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+from RideSharingapp.models import *
+
+
+
+
+class UserTblSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserTbl
+        fields = '__all__'
+        extra_kwargs = {'password':{'write_only':True}}
+
+    def create(self,validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        user = UserTbl.objects.create(**validated_data)
+        return user
 
 
 class UserSerializer(serializers.ModelSerializer):
+    username  = serializers.CharField(write_only=True)
+    password  = serializers.CharField(write_only=True)
+
     class Meta:
-        model = 'UserRegisteration'
-        field = '__all__'
+        model = UserRegisteration
+        fields = ['firstname','lastname','phonenumber','usertype','image','username','password']
+        extra_kwargs = {'password':{'write_only':True}}
+    
+    def create(self,validated_data):
+        username = validated_data.pop('username')
+        password = validated_data.pop('password')
+        userreg = UserRegisteration.objects.create(**validated_data)
+        UserTbl.objects.create(empid=userreg, username=username,password=password)  # Create UserTbl linked to UserRegisteration
+        return userreg
+       
+
+   
+
